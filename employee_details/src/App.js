b9ee09d28089da9c76ecb42a14fcd42b3812763e
ketanAys <ketan.pathtoitayssoftwaresolution.com>
2024-01-31 00:26:37 +0530

    implemented Sorting

--- employee_details/src/App.js
+++ employee_details/src/App.js
@@ -14,22 +14,6 @@ import './App.css';
 
 
 function App() {
-<<<<<<< HEAD
-    const [employees, setEmployees] = useState[{ name: "Ketan", Salary: 20000, dID: 1, Designation: "Developer" }, { name: "John", Salary: 25000, dID: 2, Designation: "Designer" }, { name: "Alice", Salary: 30000, dID: 3, Designation: "Manager" }, { name: "Bob", Salary: 22000, dID: 4, Designation: "Developer" }, { name: "Eva", Salary: 28000, dID: 5, Designation: "Tester" }, { name: "Michael", Salary: 27000, dID: 6, Designation: "Manager" }, { name: "Sophia", Salary: 24000, dID: 7, Designation: "Designer" }, { name: "William", Salary: 26000, dID: 8, Designation: "Developer" }, { name: "Emma", Salary: 29000, dID: 9, Designation: "Tester" }, { name: "Oliver", Salary: 31000, dID: 10, Designation: "Manager" }];
-
-    return ( <
-        div id = "App" >
-        <
-        Ayslogo / >
-        <
-        Header / >
-
-        <
-        EmployeeNavBar employees = { employees }
-        />  <
-        /div>
-    );
-=======
 const EmployeesData = [
 { name: "Ketan", Salary: 20000, dID: 1, Designation: "Developer" },
 { name: "John", Salary: 25000, dID: 2, Designation: "Designer" },
